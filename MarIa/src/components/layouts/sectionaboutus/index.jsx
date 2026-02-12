bd0c036aa40f4873e369d2a1af117c3514486536
nauan2024 <nauanlaurentinotec@gmail.com>
2026-02-11 22:30:23 -0300

    style:cards css

--- MarIa/src/components/layouts/sectionaboutus/index.jsx
+++ MarIa/src/components/layouts/sectionaboutus/index.jsx
@@ -10,8 +10,7 @@ export const Aboutus = () => {
                 
                 <div className='content-info'>
                     <h2>Sobre nós</h2>
-                    <p>Somos um grupo de estudantes do curso Técnico em Desenvolvimento de Sistemas movidos por um objetivo simples, mas poderoso transformar a educação ambiental em algo envolvente, divertido e acessível para todas as crianças. Criamos um aplicativo que une tecnologia, conscientização e brincadeira
-                        Nosso compromisso é contribuir para um mundo mais verde, com crianças mais informadas, conscientes e preparadas para serem protagonistas da mudança. Juntos, estamos transformando aprendizado em impacto</p>
+                    <p>Somos um grupo de estudantes do curso Técnico em Desenvolvimento de Sistemas movidos por um objetivo simples, mas poderoso transformar a educação ambiental em algo envolvente, divertido e acessível para todas as crianças. Criamos um aplicativo que une tecnologia, conscientização e brincadeira. Nosso compromisso é contribuir para um mundo mais verde, com crianças mais informadas, conscientes e preparadas para serem protagonistas da mudança. Juntos, estamos transformando aprendizado em impacto.</p>
                 </div>
             </div>
         </section>
